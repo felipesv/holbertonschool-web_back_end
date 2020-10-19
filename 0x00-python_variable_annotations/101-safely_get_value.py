7a71490f1f250324125ff9f021b9922664fb5a7b
Felipe Satizabal <1165@holbertonschool.com>
2020-10-19 13:15:32 -0500

    [fix] type

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,7 @@ T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[None, T]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
     safely get value
     """
