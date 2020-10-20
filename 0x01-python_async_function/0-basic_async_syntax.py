e01ff4529871790ffe969b9e99d85abf471e0f62
Felipe Satizabal <1165@holbertonschool.com>
2020-10-20 13:01:07 -0500

    [fix] sleep value task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,5 +11,5 @@ async def wait_random(max_delay: int = 10) -> float:
     wait_random function
     """
     randomValue = random.uniform(0, max_delay)
-    await asyncio.sleep(max_delay)
+    await asyncio.sleep(randomValue)
     return randomValue
