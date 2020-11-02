91573eb4d1ebff836219b9bdf5ea03468d267ff1
Felipe Satizabal <1165@holbertonschool.com>
2020-11-02 11:35:40 -0500

    [add] task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,6 +4,38 @@ Regex-ing
 """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Constructor
+        """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        format function
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.getLogger:
+    """
+    get_logger function
+    """
+    logging.(filename='user_data', level=logging.INFO)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
