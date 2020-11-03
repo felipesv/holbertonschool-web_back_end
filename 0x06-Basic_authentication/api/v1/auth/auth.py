b5fb55e4b1b921e4ef68c0d8fb4f05aa37f52071
Felipe Satizabal <1165@holbertonschool.com>
2020-11-03 14:53:57 -0500

    [add] task 5

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -27,7 +27,9 @@ class Auth():
         """
         authorization_header function
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
