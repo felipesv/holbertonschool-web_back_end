a1a4d5f759af57523a07476e2fdd23f7dc141446
Felipe Satizabal <1165@holbertonschool.com>
2020-11-03 17:07:44 -0500

    [add] task 10

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -55,3 +57,20 @@ class BasicAuth(Auth):
             return None, None
         values = decoded_base64_authorization_header.split(":")
         return values[0], values[1]
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """
+        user_object_from_credentials function
+        """
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        user = User.search({'email': user_email})
+        if len(user) == 0:
+            return None
+        valid_pass = user[0].is_valid_password(user_pwd)
+        if not valid_pass:
+            return None
+        return user[0]
