1d4a49db0e5cff12fb497d9d38984ba0573a4e9e
Felipe Satizabal <1165@holbertonschool.com>
2020-11-10 11:17:25 -0500

    [add] advance tasks

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -10,6 +10,8 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -21,6 +23,10 @@ if os.getenv("AUTH_TYPE") == 'basic_auth':
     auth = BasicAuth()
 elif os.getenv("AUTH_TYPE") == 'session_auth':
     auth = SessionAuth()
+elif os.getenv("AUTH_TYPE") == 'session_exp_auth':
+    auth = SessionExpAuth()
+elif os.getenv("AUTH_TYPE") == 'session_db_auth':
+    auth = SessionDBAuth()
 else:
     auth = Auth()
 
