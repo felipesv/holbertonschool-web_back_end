9a7ead8e73824b34691fca5ae12351fb45c586d0
Felipe Satizabal <1165@holbertonschool.com>
2020-11-16 13:41:34 -0500

    [fix] no type in kwargs

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -48,13 +48,11 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """
             Find an user in db
             Return the user founded
         """
-        if not kwargs:
-            raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
             raise NoResultFound
