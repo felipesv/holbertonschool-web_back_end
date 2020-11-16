f7fab0835bc8a27ec3b24bbfbef337d2db81f7c1
Felipe Satizabal <1165@holbertonschool.com>
2020-11-16 12:57:14 -0500

    [fix] documentation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -11,10 +11,14 @@ from user import User
 
 class DB:
     """
-    DB class
+        DB class
+        create model to manage the Database
     """
     def __init__(self):
-        """constructor"""
+        """
+            Constructor
+            create the object BD
+        """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -22,14 +26,20 @@ class DB:
 
     @property
     def _session(self):
-        """Create if not exists and return db session"""
+        """
+            Create if not exists a db session
+            Return the session
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add user to database"""
+        """
+            Create an User and insert in Database
+            Retrun the new User
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 """
-SQLAlchemy -> user
+Module user
+Table users with SQLAlchemy
 """
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-
-
 Base = declarative_base()
 
 
 class User(Base):
     """
-    User class
+        User class
+        create a model User from sqlalchemy for a table name users
     """
+
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
