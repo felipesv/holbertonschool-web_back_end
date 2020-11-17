4fe5e85789502d8c6192832fff9034c30bc305a3
Felipe Satizabal <1165@holbertonschool.com>
2020-11-17 13:47:28 -0500

    [add] task 13

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -83,3 +83,14 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+            Destroy a session active
+            Return None
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            pass
+        return None
