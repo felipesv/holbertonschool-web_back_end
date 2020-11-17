80ea70c22e7ad6513021cd826dca53baa8a1e5bd
Felipe Satizabal <1165@holbertonschool.com>
2020-11-17 13:40:51 -0500

    [add] task 12

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -72,3 +72,14 @@ class Auth:
             return user.session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """
+            Find user by session id
+            Return session id
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
