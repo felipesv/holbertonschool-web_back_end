2ed70da21f2ceeb3720a101271f043dc60d3a2e0
Felipe Satizabal <1165@holbertonschool.com>
2020-11-17 17:31:34 -0500

    [fix] bug

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,7 +3,6 @@
 Flask app
 """
 from flask import Flask, jsonify, request, abort, redirect
-from flask import url_for
 from auth import Auth
 
 
@@ -11,7 +10,7 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route('/',  methods=['GET'], strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def simple_get() -> str:
     """
         GET /
@@ -21,7 +20,7 @@ def simple_get() -> str:
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register_user() -> str:
+def users() -> str:
     """
         POST /users
         Return message error or success
@@ -35,8 +34,8 @@ def register_user() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route("/sessions", methods=['POST'], strict_slashes=False)
-def session() -> str:
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
     """
         Create a session user
         Return response
@@ -52,7 +51,7 @@ def session() -> str:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> None:
+def logout():
     """
         Destroy session
         Abort if id not exists or redirect
@@ -62,7 +61,7 @@ def logout() -> None:
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
-    return redirect(url_for('simple_get'))
+    return redirect('/')
 
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
