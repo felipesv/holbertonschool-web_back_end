e4999bb2aa804a62029819f38ca8c04d5639dca3
Felipe Satizabal <1165@holbertonschool.com>
2020-11-17 18:17:33 -0500

    [add] task 16

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -93,3 +93,15 @@ class Auth:
         """
         if user_id:
             self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+            Generate token to reset password
+            Return token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, reset_token=uuid.uuid4())
+            return user.reset_token
+        except NoResultFound:
+            raise ValueError
