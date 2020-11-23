31beb993c91b2a0307bacce05662998be4e88975
Felipe Satizabal <1165@holbertonschool.com>
2020-11-23 10:45:16 -0500

    [add] task 2

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,9 @@ Unittest utils.py
 Run: python3 -m unittest test_utils | tail -1
 """
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -33,3 +34,21 @@ class TestAccessNestedMap(unittest.TestCase):
         test the access_nested_map function (exception)
         """
         self.assertRaises(expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test cases
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock_class):
+        """
+        test the get_json function
+        """
+        mock_class.return_value = test_payload
+        self.assertEquals(get_json(test_url), test_payload)
