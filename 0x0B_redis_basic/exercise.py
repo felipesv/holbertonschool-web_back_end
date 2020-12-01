f642b25c92c13a821f25174a4e1d515d52169e60
Felipe Satizabal <1165@holbertonschool.com>
2020-12-01 15:02:04 -0500

    [add] task 0, 1

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+0x0B. Redis basic
+"""
+from redis.client import Redis
+from typing import Union, Callable, Optional
+import uuid
+
+
+class Cache:
+    """
+    Cache class
+    """
+
+    def __init__(self):
+        """
+        constructor
+        """
+        self._redis = Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        return data
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) ->\
+            Union[str, bytes, int, float]:
+        """
+        get function
+        """
+        if key:
+            if fn:
+                return fn(self._redis.get(key))
+            return self._redis.get(key)
