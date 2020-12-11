4839a657a9590dadfa97c3c29f641d55c9e1ff35
Felipe Satizabal <1165@holbertonschool.com>
2020-12-11 13:25:38 -0500

    [fix] slint

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = [];
   for (const idx in array) {
-    if (array.indexOf(idx)) {
-      arr.push(appendString + array[idx]);
-    }
+    arr.push(appendString + array[idx]);
   }
 
   return arr;
