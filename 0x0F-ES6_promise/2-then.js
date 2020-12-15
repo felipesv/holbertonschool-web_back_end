ed19b459a10f6772215511a3369c705a97a8ec5a
Felipe Satizabal <1165@holbertonschool.com>
2020-12-15 16:31:10 -0500

    [fix] catch error

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 /* then => (success function, error function) */
 export default function handleResponseFromAPI(promise) {
   console.log('Got a response from the API');
-  return promise.then(() => ({ status: 200, body: 'success' }), () => new Error());
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error());
 }
