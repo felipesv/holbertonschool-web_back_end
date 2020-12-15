e2dffb33fcdf7dc80d43a6f0202d0ece534d4fbb
Felipe Satizabal <1165@holbertonschool.com>
2020-12-15 16:25:15 -0500

    [add] task 0-2

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,5 @@
+/* then => (success function, error function) */
+export default function handleResponseFromAPI(promise) {
+  console.log('Got a response from the API');
+  return promise.then(() => ({ status: 200, body: 'success' }), () => new Error());
+}
