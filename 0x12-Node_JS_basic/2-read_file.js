5a32c52a7fb3b5de43159ebf4db259391b61621a
Felipe Satizabal <1165@holbertonschool.com>
2021-01-06 12:56:54 -0500

    [fix] conditions

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,27 +1,31 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
+  const fields = {};
+  let data;
+
   try {
-    const data = fs.readFileSync(path, 'utf8').split('\n');
-    const fields = {};
-    data.shift();
-    data.forEach((element) => {
-      const row = element.split(',');
-      if (row[3] in fields) {
-        fields[row[3]].push(row[0]);
-      } else {
-        fields[row[3]] = [row[0]];
-      }
-    });
-    console.log(`Number of students: ${data.length}`);
-    for (const field in fields) {
-      if (field) {
-        const list = fields[field];
-        console.log(`Number of students in ${field}: ${list.length}. List: ${list.toString().replace(/,/g, ', ')}`);
-      }
-    }
+    data = fs.readFileSync(path);
   } catch (error) {
     throw new Error('Cannot load the database');
+  }
+
+  data = data.toString().split('\n');
+  data.shift();
+  data.forEach((element) => {
+    const row = element.split(',');
+    if (row[3] in fields) {
+      fields[row[3]].push(row[0]);
+    } else {
+      fields[row[3]] = [row[0]];
+    }
+  });
+  console.log(`Number of students: ${data.length}`);
+  for (const field in fields) {
+    if (field) {
+      const list = fields[field];
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.toString().replace(/,/g, ', ')}`);
+    }
   }
 };
 
