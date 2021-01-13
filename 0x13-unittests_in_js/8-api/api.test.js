095301eb69998803cf6e79fce44f29f357a6c5cd
Felipe Satizabal <1165@holbertonschool.com>
2021-01-12 20:56:04 -0500

    [fix] order to test, implement done

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,14 @@
 const { expect } = require('chai');
 const request = require('request');
 
-describe('API 8', () => {
-  it('request', () => {
+describe('Index page', () => {
+  it('request', (done) => {
     request('http://localhost:7865', (error, response, body) => {
       if (response) {
-        expect(body).to.equals('Welcome to the payment system');
         expect(response.statusCode).to.equal(200);
         expect(response.statusMessage).to.equal('OK');
+        expect(body).to.equals('Welcome to the payment system');
+        done();
       }
     });
   });
